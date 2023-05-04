0236d43b2d8857343b9396e0fe53c14cc5ea2642
leseraphina <leseraphina@gamil.com>
2023-05-04 13:20:49 +0900

    react-basic

--- src/index.js
+++ src/index.js
@@ -1,17 +1,61 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const people ={
+  no:1,
+  level: '1단계',
+  room: 'A강의실'
+}
+
+
+function Header(){
+  return (
+    <header>
+      <h1>{`React Welcome + ${1 + 2}`}</h1>
+    </header>
+  );
+}
+
+function Content(){
+  return (
+    <>
+    <ul className="main" name={5}>
+      <li>{people.no}</li>
+      <li>{people.level}</li>
+      <li>{people.room}</li>
+    </ul>
+    <p> 
+      <span>react 컴포넌트 내용</span>
+      <label htmlFor="userName">네임</label>
+      <input type="text" id="userName" />
+    </p>
+    </>
+  );
+}
+
+function Footer(){
+  return (
+    <footer>
+      <small>저작권정보</small>
+    </footer>
+  );
+}
+
+
+function App(){
+  return (
+    <div>
+      <Header />
+      <Content />
+      <Footer />
+    </div>
+  )
+}
+
+// export
+const root = ReactDOM.createRoot(document.querySelector('#root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
